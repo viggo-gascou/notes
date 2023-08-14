41073c0032037836a7d74c97954c710dff24e273
viggo-gascou <94069687+viggo-gascou@users.noreply.github.com>
2023-08-14 15:13:31 +0200

    ✨ feat: initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,34 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, sharpImageService } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 import mdx from '@astrojs/mdx';
 
-import sitemap from '@astrojs/sitemap';
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
+
+
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://example.com',
+    integrations: [sitemap(), tailwind(), react(), mdx()],
+    markdown: {
+        remarkPlugins: [
+            remarkMath
+        ],
+        rehypePlugins: [
+            [rehypeKatex, { 
+                macros: {
+
+                },
+            }]
+    ],
+    },
+    experimental: {
+        assets: true,
+      },
+      image: {
+        service: sharpImageService(),
+      },
 });
