ea96a4f4e151c9f14f5bc1659402a54ee76448f0
viggo-gascou <94069687+viggo-gascou@users.noreply.github.com>
2023-08-18 20:22:56 +0200

    ✨ feat: update constants

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Viggo Gascou';
-export const SITE_DESCRIPTION = 'Welcome to my notes!';
+export const SITE_DESCRIPTION = 'Hi, I\'m Viggo Gascou. I\'m a student data scientist at the IT University of Copenhagen. I\'m interested in machine learning, data visualization, and web development.';
