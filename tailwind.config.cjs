9bb1a40873d2d9c312a3b6b70eccd0f99fa0ac57
viggo-gascou <94069687+viggo-gascou@users.noreply.github.com>
2023-08-18 20:21:28 +0200

    🐛 fix: update imports and various style changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,40 +1,34 @@
-/** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme')
 
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: "class",
 	theme: {
 		extend: {
-      colors: {
-        'shark': {
-          '50': 'hsl(240, 20%, 97%)',
-          '100': 'hsl(240, 12%, 94%)',
-          '200': 'hsl(233, 13%, 86%)',
-          '300': 'hsl(233, 13%, 74%)',
-          '400': 'hsl(235, 13%, 60%)',
-          '500': 'hsl(234, 12%, 48%)',
-          '600': 'hsl(236, 13%, 39%)',
-          '700': 'hsl(237, 13%, 32%)',
-          '800': 'hsl(236, 11%, 27%)',
-          '900': 'hsl(236, 11%, 24%)',
-          '950': 'hsl(240, 11%, 11%)',
-      },
-      
-  
-    },
-			fontfamily: {
-                'sans': ["'Atkinson Hyperlegible'", ...defaultTheme.fontFamily.sans],
-            },
+			colors: {
+			'shark': {
+				'50': 'hsl(240, 20%, 97%)',
+				'100': 'hsl(240, 12%, 94%)',
+				'200': 'hsl(233, 13%, 86%)',
+				'300': 'hsl(233, 13%, 74%)',
+				'400': 'hsl(235, 13%, 60%)',
+				'500': 'hsl(234, 12%, 48%)',
+				'600': 'hsl(236, 13%, 39%)',
+				'700': 'hsl(237, 13%, 32%)',
+				'800': 'hsl(236, 11%, 27%)',
+				'900': 'hsl(236, 11%, 24%)',
+				'950': 'hsl(240, 11%, 11%)',
+			},
+			'tag': '#96bafa',
+			'accent': 'hsl(225deg, 88%, 68%)',
+
+    		},
 			boxShadow: {
-				wglow: [
-					"0 0px 20px rgba(255,255, 255, 0.35)",
-					"0 0px 65px rgba(255, 255,255, 0.2)",
-				  ],
-				bglow: [
-					"0 0px 20px rgba(0,0, 0, 0.35)",
-					"0 0px 65px rgba(0, 0,0, 0.2)",
-				  ],
+				accent: [
+					"0 0px 10px hsl(225deg, 88%, 68%)",
+				],
 			},
 			typography:  (theme) => ({
 				DEFAULT: {
@@ -44,6 +38,9 @@ module.exports = {
 					  },
 					  'code::after': {
 						content: '""'
+					  },
+					  h1: {
+						marginBottom: 0,
 					  },
             code: {
               color: theme('colors.slate.500'),
