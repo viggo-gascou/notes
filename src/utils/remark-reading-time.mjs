5e197acca7fff594c33d43fc062b1f67bef008f0
viggo-gascou <94069687+viggo-gascou@users.noreply.github.com>
2023-08-20 14:41:11 +0200

    ♻️ refactor: remove console.log

--- src/utils/remark-reading-time.mjs
+++ src/utils/remark-reading-time.mjs
@@ -9,6 +9,5 @@ export function remarkReadingTime() {
     // readingTime.text will give us minutes read as a friendly string,
     // i.e. "3 min read"
     data.astro.frontmatter.minutesRead = readingTime.text;
-    console.log(readingTime)
   };
 }
