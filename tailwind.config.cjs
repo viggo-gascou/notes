9688c75cbfddaef99a896b5e6ae0760e50355bec
viggo-gascou <94069687+viggo-gascou@users.noreply.github.com>
2023-08-20 14:42:01 +0200

    ✨ feat: disable hover on mobile because broken

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,9 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: "class",
+	future: {
+		hoverOnlyWhenSupported: true,
+	},
 	theme: {
 		extend: {
 			colors: {
