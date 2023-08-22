8d43d3703be6abb41d4ff805e1e0712cf04c0a1a
viggo-gascou <94069687+viggo-gascou@users.noreply.github.com>
2023-08-22 20:06:18 +0200

    ♻️ refactor: various minor fixes and updates

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useSpring, animated } from "react-spring";
+import { useSpring, animated } from "@react-spring/web";
 
 
 export default function DarkModeToggle() {
